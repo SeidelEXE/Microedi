5f1800e67944cb776b5182dd3c1ba3050a4688a7
SeidelEXE <gabriellucasseidel@outlook.com>
2025-08-12 20:21:30 -0300

    update

--- microedi/src/Componentes/Conteudo.jsx
+++ microedi/src/Componentes/Conteudo.jsx
@@ -1,11 +1,16 @@
+import './Conteudo.css'
+
 export default function Conteudo(){
     return(
         <>
-        <div id="conteudo">
-            <h1>
-            Informática e tecnologia,
-            tudo em um só lugar</h1>
-            A Microedi é o lugar certo para manutenções de notebooks e PCs, compras de computadores já montados, acessórios e gadgets. Honestidade, confiança e preço justo com o melhor atendimento da região!
+        <div className="conteudo-wrapper">
+            <div className="conteudo">
+                <h1>
+                Informática e tecnologia,
+                tudo em um só lugar</h1>
+                A Microedi é o lugar certo para manutenções de notebooks e PCs, compras de computadores já montados, acessórios e gadgets. Honestidade, confiança e preço justo com o melhor atendimento da região!
+            </div>
+            <img className="mulher-sentada" src="/unnamed.png" alt="" />
         </div>
         </>
     )
