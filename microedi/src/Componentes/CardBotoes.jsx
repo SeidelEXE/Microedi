31492f736d27157b8ffd6f4183aa54ce2bcdda38
SeidelEXE <gabriellucasseidel@outlook.com>
2025-08-18 19:53:45 -0300

    update

--- microedi/src/Componentes/CardBotoes.jsx
+++ microedi/src/Componentes/CardBotoes.jsx
@@ -3,13 +3,17 @@ import './CardBotoes.css'
 export default function CardBotoes(){
     return(
         <>
+        <img src="../assets/icons/2001.svg"  alt="" />
         <div className='container-cards'>
-            <button>Button</button>
+            <button id='transparencia'>
+                
+                Transparência
+                </button>
             <button id='marcado'>Desde 2001</button>
-            <button>Button</button> <br />
-            <button>Button</button>
-            <button>Button</button>
-            <button>Button</button>
+            <button id='expertise'>Expertise</button> <br />
+            <button id='solucao'>Soluções</button>
+            <button id='seguranca'>Segurança</button>
+            <button id='preco-justo'>Preço Justo</button>
         </div>
         </>
     )
