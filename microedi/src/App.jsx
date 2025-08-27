dac3e9eaebcc4edce6b5d804672ec45bb0ffd962
SeidelEXE <gabriellucasseidel@outlook.com>
2025-08-27 20:32:55 -0300

    routers pkg

--- microedi/src/App.jsx
+++ microedi/src/App.jsx
@@ -3,8 +3,7 @@ import './App.css'
 import Cabecalho from './Componentes/Cabecalho'
 import Conteudo from './Componentes/Conteudo'
 import Rodape from './Componentes/Rodape'
-//import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
-
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
 function App() {
   const [count, setCount] = useState(0)
